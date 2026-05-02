034ff673d679f560d173df8f07b5f2f13a0a9cfd
9001498 <9001498@student.zadkine.nl>
2026-05-02 18:53:41 +0200

    Start web app from desktop wrapper

--- SignageAdmin.Desktop/MainWindow.xaml.cs
+++ SignageAdmin.Desktop/MainWindow.xaml.cs
@@ -1,14 +1,55 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace SignageAdmin.Desktop;
 
 public partial class MainWindow : Window
 {
+    private Process? _webProcess;
+
     public MainWindow()
     {
         InitializeComponent();
 
+        Loaded += MainWindow_Loaded;
+        Closed += MainWindow_Closed;
+    }
+
+    private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        StartWebApp();
+
+        await Task.Delay(3000);
+
         Browser.Source = new Uri("http://localhost:5278");
+    }
+
+    private void StartWebApp()
+    {
+        var webProjectPath = Path.GetFullPath(
+            Path.Combine(AppContext.BaseDirectory, @"..\..\..\..\SignageAdmin.Web.csproj")
+        );
+
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = "dotnet",
+            Arguments = $"run --project \"{webProjectPath}\"",
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        _webProcess = Process.Start(startInfo);
+    }
+
+    private void MainWindow_Closed(object? sender, EventArgs e)
+    {
+        if (_webProcess is not null && !_webProcess.HasExited)
+        {
+            _webProcess.Kill(true);
+            _webProcess.Dispose();
+        }
     }
 }
