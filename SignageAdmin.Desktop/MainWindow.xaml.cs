13d334bfdf6e9714883d6cf1923c3fea5ca5323f
9001498 <9001498@student.zadkine.nl>
2026-05-02 18:47:14 +0200

    Show web admin in desktop WebView2 wrapper

--- SignageAdmin.Desktop/MainWindow.xaml.cs
+++ SignageAdmin.Desktop/MainWindow.xaml.cs
@@ -1,23 +1,14 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SignageAdmin.Desktop;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+
+        Browser.Source = new Uri("http://localhost:5278");
     }
 }
